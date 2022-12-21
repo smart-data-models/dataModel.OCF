44c28ed077343922c8e7700787211ecbf36c376e
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:15:24 +0100

    test of SQL export

---
+++ Torque/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Torque of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Torque_type AS ENUM ('Torque');
+CREATE TABLE Torque (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, range json, rt json, seeAlso json, source text, step text, torque text, type Torque_type);
