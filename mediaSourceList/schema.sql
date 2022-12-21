5807dd7014effea1dd204f14ae0f180b8327c280
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:14:50 +0100

    test of SQL export

---
+++ mediaSourceList/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model mediaSourceList of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE mediaSourceList_type AS ENUM ('mediaSourceList');
+CREATE TABLE mediaSourceList (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, rt json, seeAlso json, source text, sources json, type mediaSourceList_type);
