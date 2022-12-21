0fc935b3a614c28763fcbb434a52d7724a152fc1
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:15:41 +0100

    test of SQL export

---
+++ AutoFocus/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model AutoFocus of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE AutoFocus_type AS ENUM ('AutoFocus');
+CREATE TABLE AutoFocus (address json, alternateName text, areaServed text, autoFocus text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, rt json, seeAlso json, source text, type AutoFocus_type);
