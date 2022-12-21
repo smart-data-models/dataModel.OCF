2c44b5dd5d86e4a5bab293b76b88b4e1e6322f39
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:15:22 +0100

    test of SQL export

---
+++ TimeStamp/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model TimeStamp of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE TimeStamp_type AS ENUM ('TimeStamp');
+CREATE TABLE TimeStamp (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, rt json, seeAlso json, source text, timestamp timestamp, type TimeStamp_type);
