506c54b553852fde619a52bfa02f2124473cb463
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:13:45 +0100

    test of SQL export

---
+++ Cadence/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Cadence of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Cadence_type AS ENUM ('Cadence');
+CREATE TABLE Cadence (address json, alternateName text, areaServed text, cadence integer, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, range json, rt json, seeAlso json, source text, step integer, type Cadence_type);
