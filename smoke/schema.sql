f623ef0289dd2b77bcfb6de076f85b656c6c58be
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:15:11 +0100

    test of SQL export

---
+++ smoke/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model smoke of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE smoke_type AS ENUM ('smoke');
+CREATE TABLE smoke (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, measurement text, n text, name text, owner json, precision text, range json, rt json, seeAlso json, source text, step text, type smoke_type, value text);
