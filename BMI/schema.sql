e91926fd51422cd5d188e7449ca893b695a5ac59
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:13:34 +0100

    test of SQL export

---
+++ BMI/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model BMI of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE BMI_type AS ENUM ('BMI');
+CREATE TABLE BMI (address json, alternateName text, areaServed text, bmi text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, precision text, range json, rt json, seeAlso json, source text, step text, type BMI_type);
