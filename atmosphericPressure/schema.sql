99d20d18b3cc2903fc8f06c1ab498b61da1bf3d1
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:13:30 +0100

    test of SQL export

---
+++ atmosphericPressure/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model atmosphericPressure of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE atmosphericPressure_type AS ENUM ('atmosphericPressure');
+CREATE TABLE atmosphericPressure (address json, alternateName text, areaServed text, atmosphericPressure text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, precision text, range json, rt json, seeAlso json, source text, step text, type atmosphericPressure_type);
