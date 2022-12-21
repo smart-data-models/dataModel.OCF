91b443ccb84ad99f4e28f34a1ea6d43ae2d1b9fd
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:13:57 +0100

    test of SQL export

---
+++ ColourRGB/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ColourRGB of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ColourRGB_type AS ENUM ('ColourRGB');
+CREATE TABLE ColourRGB (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, range json, rgbValue json, rt json, seeAlso json, source text, step integer, type ColourRGB_type);
