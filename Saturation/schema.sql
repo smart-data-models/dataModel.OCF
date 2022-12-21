60c652dcfc16dd6ba2772755595da9974ef04fe0
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:17:24 +0100

    test of SQL export

---
+++ Saturation/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Saturation of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Saturation_type AS ENUM ('Saturation');
+CREATE TABLE Saturation (address json, alternateName text, areaServed text, colourSaturation integer, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, rt json, seeAlso json, source text, type Saturation_type);
