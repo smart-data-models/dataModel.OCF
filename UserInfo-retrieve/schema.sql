774462a28dcc2445d1690fe255c2ca7998ef1dcf
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:17:53 +0100

    test of SQL export

---
+++ UserInfo-retrieve/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model UserInfo-retrieve of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE UserInfo-retrieve_type AS ENUM ('UserInfo-retrieve');
+CREATE TABLE UserInfo-retrieve (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, rt json, seeAlso json, source text, type UserInfo-retrieve_type);
