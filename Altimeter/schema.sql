d75e7da6f9cddffddb3f9e46f035178bd3f4b2ff
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:13:28 +0100

    test of SQL export

---
+++ Altimeter/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Altimeter of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Altimeter_type AS ENUM ('Altimeter');
+CREATE TABLE Altimeter (address json, alt text, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, precision text, range json, rt json, seeAlso json, source text, step text, type Altimeter_type);
