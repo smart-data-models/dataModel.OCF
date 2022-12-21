36e9a4eb40f0b6bdd64cd26e1d05d6252c3c0a3b
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:14:15 +0100

    test of SQL export

---
+++ ContinuousGlucoseMeterThreshold/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ContinuousGlucoseMeterThreshold of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ContinuousGlucoseMeterThreshold_type AS ENUM ('ContinuousGlucoseMeterThreshold');
+CREATE TABLE ContinuousGlucoseMeterThreshold (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, dhyper text, dhypo text, gdr text, gir text, id text, if json, location json, n text, name text, owner json, phigh text, plow text, precision text, range json, rt json, seeAlso json, source text, step text, type ContinuousGlucoseMeterThreshold_type);
