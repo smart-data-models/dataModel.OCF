61b942b4417aa8afe12cbab6c9658c6c5f4b66db
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:14:02 +0100

    test of SQL export

---
+++ Consumption/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Consumption of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Consumption_type AS ENUM ('Consumption');
+CREATE TABLE Consumption (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, gas text, id text, if json, location json, n text, name text, owner json, precision text, rt json, seeAlso json, source text, type Consumption_type, volume text);
