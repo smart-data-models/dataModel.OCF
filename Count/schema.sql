b4d1e1f0672ea1d086f09af464d9b0b14a66d3ab
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:16:14 +0100

    test of SQL export

---
+++ Count/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Count of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Count_type AS ENUM ('Count');
+CREATE TABLE Count (address json, alternateName text, areaServed text, count integer, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, range json, rt json, seeAlso json, source text, step integer, type Count_type);
