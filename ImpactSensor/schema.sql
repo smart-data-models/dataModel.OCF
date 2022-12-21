36b2479e843ca1dcca290c65e7a15abdab0b9773
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:16:42 +0100

    test of SQL export

---
+++ ImpactSensor/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ImpactSensor of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ImpactSensor_type AS ENUM ('ImpactSensor');
+CREATE TABLE ImpactSensor (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, impactdirectionhorizontal text, impactdirectionvertical text, impactlevel text, impactstatus text, location json, n text, name text, owner json, rt json, seeAlso json, source text, type ImpactSensor_type);
