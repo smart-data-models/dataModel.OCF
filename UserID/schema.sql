759a83a87160e22088e0f57ebceaf098b4cafca4
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:15:27 +0100

    test of SQL export

---
+++ UserID/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model UserID of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE UserID_type AS ENUM ('UserID');
+CREATE TABLE UserID (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, rt json, seeAlso json, source text, type UserID_type, userid text);
