9650815ba66ba811e387113d72e4f68586c93919
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:14:59 +0100

    test of SQL export

---
+++ PulseRate/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model PulseRate of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE PulseRate_type AS ENUM ('PulseRate');
+CREATE TABLE PulseRate (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, pulserate integer, range json, rt json, seeAlso json, source text, step integer, type PulseRate_type);
