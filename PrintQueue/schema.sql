4a5c00b1d826b9b06b1b3940160b358bee43c465
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:00:51 +0200

    test of SQL export

--- PrintQueue/schema.sql
+++ PrintQueue/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model PrintQueue of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model PrintQueue of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PrintQueue_type AS ENUM ('PrintQueue');
-CREATE TABLE PrintQueue (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, queue json, rt json, seeAlso json, source text, type PrintQueue_type);
+CREATE TABLE PrintQueue (if JSON, n TEXT, queue JSON, rt JSON, type PrintQueue_type);
