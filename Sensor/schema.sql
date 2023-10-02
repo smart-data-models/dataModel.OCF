b17a502a9d659b93d5015ac6c6a7012637c05b00
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:58:27 +0200

    test of SQL export

--- Sensor/schema.sql
+++ Sensor/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Sensor of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Sensor of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Sensor_type AS ENUM ('Sensor');
-CREATE TABLE Sensor (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, measurement text, n text, name text, owner json, precision text, range json, rt json, seeAlso json, source text, step text, type Sensor_type, value text);
+CREATE TABLE Sensor (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, if JSON, measurement NUMERIC, n TEXT, name TEXT, owner JSON, precision NUMERIC, range JSON, rt JSON, source TEXT, step NUMERIC, type Sensor_type, value BOOLEAN);
