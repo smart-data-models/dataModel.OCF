3ed5b805925994bd52bdebd895af9b51befdb88a
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:00:28 +0200

    test of SQL export

--- magneticFieldDirection/schema.sql
+++ magneticFieldDirection/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model magneticFieldDirection of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model magneticFieldDirection of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE magneticFieldDirection_type AS ENUM ('magneticFieldDirection');
-CREATE TABLE magneticFieldDirection (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, rt json, seeAlso json, source text, type magneticFieldDirection_type, value json);
+CREATE TABLE magneticFieldDirection (if JSON, n TEXT, rt JSON, type magneticFieldDirection_type, value JSON);
