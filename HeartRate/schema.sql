17c4d425e651789c67d8d85c93b49ce39842dd91
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:57:57 +0200

    test of SQL export

--- HeartRate/schema.sql
+++ HeartRate/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model HeartRate of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model HeartRate of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE HeartRate_type AS ENUM ('HeartRate');
-CREATE TABLE HeartRate (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, heartrate integer, id text, if json, location json, n text, name text, owner json, range json, rt json, seeAlso json, source text, step integer, type HeartRate_type);
+CREATE TABLE HeartRate (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, heartrate NUMERIC, if JSON, n TEXT, name TEXT, owner JSON, range JSON, rt JSON, source TEXT, step NUMERIC, type HeartRate_type);
