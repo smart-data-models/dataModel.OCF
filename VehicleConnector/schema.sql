0f087b8fc4e7be4b99dc546ed36bda44e740a7c8
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:58:54 +0200

    test of SQL export

--- VehicleConnector/schema.sql
+++ VehicleConnector/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model VehicleConnector of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model VehicleConnector of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE VehicleConnector_type AS ENUM ('VehicleConnector');
-CREATE TABLE VehicleConnector (address json, alternateName text, areaServed text, connected text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, ratedchargingcapacity text, rateddischargingcapacity text, rt json, seeAlso json, source text, type VehicleConnector_type);
+CREATE TABLE VehicleConnector (address JSON, alternateName TEXT, areaServed TEXT, connected BOOLEAN, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, if JSON, n TEXT, name TEXT, owner JSON, ratedchargingcapacity NUMERIC, rateddischargingcapacity NUMERIC, rt JSON, source TEXT, type VehicleConnector_type);
