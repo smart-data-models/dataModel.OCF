a6d6f1aff97a02d32eee8b3ece60d11fabc2688d
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:57:40 +0200

    test of SQL export

--- Foaming/schema.sql
+++ Foaming/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Foaming of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Foaming of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Foaming_type AS ENUM ('Foaming');
-CREATE TABLE Foaming (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, foamstrength integer, id text, if json, location json, n text, name text, owner json, range json, rt json, seeAlso json, source text, step integer, type Foaming_type);
+CREATE TABLE Foaming (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, foamstrength NUMERIC, if JSON, n TEXT, name TEXT, owner JSON, range JSON, rt JSON, source TEXT, step NUMERIC, type Foaming_type);
