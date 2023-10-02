0f18652ff3bd0f352be6921cf7ad0f6ea5d1e205
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:00:02 +0200

    test of SQL export

--- GlucoseMeal/schema.sql
+++ GlucoseMeal/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model GlucoseMeal of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
-CREATE TYPE meal_type AS ENUM ('preprandial', 'postprandial', 'fasting', 'bedtime', 'casual');CREATE TYPE GlucoseMeal_type AS ENUM ('GlucoseMeal');
-CREATE TABLE GlucoseMeal (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, meal meal_type, n text, name text, owner json, rt json, seeAlso json, source text, type GlucoseMeal_type);
+/* (Beta) Export of data model GlucoseMeal of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE meal_type AS ENUM ('preprandial','postprandial','fasting','bedtime','casual');CREATE TYPE GlucoseMeal_type AS ENUM ('GlucoseMeal');
+CREATE TABLE GlucoseMeal (if JSON, meal meal_type, n TEXT, rt JSON, type GlucoseMeal_type);
