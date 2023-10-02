eaa1d355216580af0bc7b9ce2ee404707c4d7df3
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:57:54 +0200

    test of SQL export

--- HbA1c/schema.sql
+++ HbA1c/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model HbA1c of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model HbA1c of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE HbA1c_type AS ENUM ('HbA1c');
-CREATE TABLE HbA1c (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hba1c text, id text, if json, location json, n text, name text, owner json, precision text, range json, rt json, seeAlso json, source text, step text, type HbA1c_type);
+CREATE TABLE HbA1c (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hba1c NUMERIC, if JSON, n TEXT, name TEXT, owner JSON, precision NUMERIC, range JSON, rt JSON, source TEXT, step NUMERIC, type HbA1c_type);
