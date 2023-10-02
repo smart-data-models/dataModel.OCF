416236d2e9359cb9c8fd9da1973842b7a5154a08
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:00:37 +0200

    test of SQL export

--- Motion/schema.sql
+++ Motion/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Motion of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Motion of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Motion_type AS ENUM ('Motion');
-CREATE TABLE Motion (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, rt json, seeAlso json, source text, type Motion_type, value text);
+CREATE TABLE Motion (if JSON, n TEXT, rt JSON, type Motion_type, value BOOLEAN);
