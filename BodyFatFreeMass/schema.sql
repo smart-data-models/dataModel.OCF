3f08fb4340227354a4c237674c5932aaa73f14f1
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:56:49 +0200

    test of SQL export

--- BodyFatFreeMass/schema.sql
+++ BodyFatFreeMass/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model BodyFatFreeMass of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
-CREATE TYPE BodyFatFreeMass_type AS ENUM ('BodyFatFreeMass');CREATE TYPE units_type AS ENUM ('kg', 'lb', 'percent');
-CREATE TABLE BodyFatFreeMass (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, ffm text, id text, if json, location json, n text, name text, owner json, precision text, range json, rt json, seeAlso json, source text, step text, type BodyFatFreeMass_type, units units_type);
+/* (Beta) Export of data model BodyFatFreeMass of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE BodyFatFreeMass_type AS ENUM ('BodyFatFreeMass');CREATE TYPE units_type AS ENUM ('kg','lb','percent');
+CREATE TABLE BodyFatFreeMass (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, ffm NUMERIC, if JSON, n TEXT, name TEXT, owner JSON, precision NUMERIC, range JSON, rt JSON, source TEXT, step NUMERIC, type BodyFatFreeMass_type, units units_type);
