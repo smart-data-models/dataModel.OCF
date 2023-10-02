24b54a0f694f8d7670fb9e48ef6a9ac09e2c4776
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:01:43 +0200

    test of SQL export

--- UVRadiation/schema.sql
+++ UVRadiation/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model UVRadiation of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model UVRadiation of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE UVRadiation_type AS ENUM ('UVRadiation');
-CREATE TABLE UVRadiation (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, measurement text, n text, name text, owner json, rt json, seeAlso json, source text, type UVRadiation_type);
+CREATE TABLE UVRadiation (if JSON, measurement NUMERIC, n TEXT, rt JSON, type UVRadiation_type);
