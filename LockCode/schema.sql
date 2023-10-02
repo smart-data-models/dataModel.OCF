fc1cd7cd1a486a5b009c28b0e2e6f43ba152fa22
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:00:25 +0200

    test of SQL export

--- LockCode/schema.sql
+++ LockCode/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model LockCode of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model LockCode of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE LockCode_type AS ENUM ('LockCode');
-CREATE TABLE LockCode (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, lockCodeList json, n text, name text, owner json, rt json, seeAlso json, source text, type LockCode_type);
+CREATE TABLE LockCode (if JSON, lockCodeList JSON, n TEXT, rt JSON, type LockCode_type);
