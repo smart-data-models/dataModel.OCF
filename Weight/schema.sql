7683525d02088430398a548b2bae5bd4a0693e5c
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:58:58 +0200

    test of SQL export

--- Weight/schema.sql
+++ Weight/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Weight of the subject dataModel.OCF for a postgreSQL database. Pending translation of enumerations and multityped attributes */
-CREATE TYPE Weight_type AS ENUM ('Weight');CREATE TYPE units_type AS ENUM ('kg', 'g', 'lb', 'oz');
-CREATE TABLE Weight (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, if json, location json, n text, name text, owner json, precision text, range json, rt json, seeAlso json, source text, step text, type Weight_type, units units_type, weight text);
+/* (Beta) Export of data model Weight of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Weight_type AS ENUM ('Weight');CREATE TYPE units_type AS ENUM ('kg','g','lb','oz');
+CREATE TABLE Weight (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, if JSON, n TEXT, name TEXT, owner JSON, precision NUMERIC, range JSON, rt JSON, source TEXT, step NUMERIC, type Weight_type, units units_type, weight NUMERIC);
