e04f435dc5c95e8d9ee014799b09a7589c8f368d
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:09:41 +0200

    test of SQL export

--- Water/schema.sql
+++ Water/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Water of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Water_type AS ENUM ('Water');
-CREATE TABLE Water (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, if JSON, measurement NUMERIC, n TEXT, name TEXT, owner JSON, precision NUMERIC, range JSON, rt JSON, source TEXT, step NUMERIC, type Water_type, value BOOLEAN);
+CREATE TABLE Water (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, measurement NUMERIC, n TEXT, name TEXT, owner JSON, precision NUMERIC, range JSON, rt JSON, seeAlso JSON, source TEXT, step NUMERIC, type Water_type, value BOOLEAN);
