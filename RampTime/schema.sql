ff8663cecb282ddb2abdeb6baa2caf168d9a43ac
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:09:09 +0200

    test of SQL export

--- RampTime/schema.sql
+++ RampTime/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model RampTime of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE RampTime_type AS ENUM ('RampTime');
-CREATE TABLE RampTime (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, if JSON, n TEXT, name TEXT, owner JSON, precision NUMERIC, rampTime NUMERIC, range JSON, rt JSON, source TEXT, step NUMERIC, type RampTime_type);
+CREATE TABLE RampTime (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, owner JSON, precision NUMERIC, rampTime NUMERIC, range JSON, rt JSON, seeAlso JSON, source TEXT, step NUMERIC, type RampTime_type);
