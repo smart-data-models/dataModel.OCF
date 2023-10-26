04ab8f7182125b168c450277419f4ec58f4b1ec4
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:08:56 +0200

    test of SQL export

--- OpaqueData/schema.sql
+++ OpaqueData/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model OpaqueData of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE OpaqueData_type AS ENUM ('OpaqueData');
-CREATE TABLE OpaqueData (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, encoding TEXT, hash TEXT, if JSON, name TEXT, owner JSON, payload TEXT, payloadtype TEXT, rt JSON, size NUMERIC, source TEXT, system TEXT, type OpaqueData_type);
+CREATE TABLE OpaqueData (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, encoding TEXT, hash TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, name TEXT, owner JSON, payload TEXT, payloadtype TEXT, rt JSON, seeAlso JSON, size NUMERIC, source TEXT, system TEXT, type OpaqueData_type);
