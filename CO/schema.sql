a7144f9b581e410c70da0d30610bd6ac01cec588
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:07:52 +0200

    test of SQL export

--- CO/schema.sql
+++ CO/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model CO of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE CO_type AS ENUM ('CO');
-CREATE TABLE CO (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, if JSON, measurement NUMERIC, n TEXT, name TEXT, owner JSON, precision NUMERIC, range JSON, rt JSON, source TEXT, step NUMERIC, type CO_type, value BOOLEAN);
+CREATE TABLE CO (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, measurement NUMERIC, n TEXT, name TEXT, owner JSON, precision NUMERIC, range JSON, rt JSON, seeAlso JSON, source TEXT, step NUMERIC, type CO_type, value BOOLEAN);
