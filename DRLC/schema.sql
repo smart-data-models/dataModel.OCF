72bb5c3b0c6133c1a7aca84753234c651243c1f9
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:08:23 +0200

    test of SQL export

--- DRLC/schema.sql
+++ DRLC/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DRLC of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DRLC_type AS ENUM ('DRLC');
-CREATE TABLE DRLC (DRType NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, drlevel NUMERIC, duration NUMERIC, if JSON, mandate BOOLEAN, n TEXT, name TEXT, override BOOLEAN, owner JSON, rt JSON, source TEXT, start TIMESTAMP, type DRLC_type);
+CREATE TABLE DRLC (DRType NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, drlevel NUMERIC, duration NUMERIC, id TEXT PRIMARY KEY, if JSON, location JSON, mandate BOOLEAN, n TEXT, name TEXT, override BOOLEAN, owner JSON, rt JSON, seeAlso JSON, source TEXT, start TIMESTAMP, type DRLC_type);
