872fe1319c07c7db7357588e43b52c3081d36110
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:08:05 +0200

    test of SQL export

--- Contact/schema.sql
+++ Contact/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Contact of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Contact_type AS ENUM ('Contact');
-CREATE TABLE Contact (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, if JSON, n TEXT, name TEXT, owner JSON, rt JSON, source TEXT, type Contact_type, value BOOLEAN);
+CREATE TABLE Contact (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, type Contact_type, value BOOLEAN);
