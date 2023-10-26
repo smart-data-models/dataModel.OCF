2fb7a6f753e308b00c44714ae01421483011cb7e
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:08:59 +0200

    test of SQL export

--- OpenLevel/schema.sql
+++ OpenLevel/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model OpenLevel of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE OpenLevel_type AS ENUM ('OpenLevel');
-CREATE TABLE OpenLevel (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, if JSON, n TEXT, name TEXT, openLevel NUMERIC, owner JSON, range JSON, rt JSON, source TEXT, step NUMERIC, type OpenLevel_type);
+CREATE TABLE OpenLevel (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, openLevel NUMERIC, owner JSON, range JSON, rt JSON, seeAlso JSON, source TEXT, step NUMERIC, type OpenLevel_type);
