a0974f68458f168f9edf5e01121a291772086ba1
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:09:24 +0200

    test of SQL export

--- threeAxis/schema.sql
+++ threeAxis/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model threeAxis of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE threeAxis_type AS ENUM ('threeAxis');
-CREATE TABLE threeAxis (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, if JSON, n TEXT, name TEXT, orientation JSON, owner JSON, rt JSON, source TEXT, type threeAxis_type);
+CREATE TABLE threeAxis (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, orientation JSON, owner JSON, rt JSON, seeAlso JSON, source TEXT, type threeAxis_type);
