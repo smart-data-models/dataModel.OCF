1cd7ceabb170c3aa249fb50c0041871ef5e41fe3
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:06:53 +0100

    test of SQL export

--- GlassBreak/schema.sql
+++ GlassBreak/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model GlassBreak of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE GlassBreak_type AS ENUM ('GlassBreak');
-CREATE TABLE GlassBreak (if JSON, n TEXT, rt JSON, type GlassBreak_type, value BOOLEAN);
+CREATE TABLE GlassBreak (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, type GlassBreak_type, value BOOLEAN);
