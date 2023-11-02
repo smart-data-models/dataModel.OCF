f7342294dc08b539527561d617b1f677b7a19d91
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:07:57 +0100

    test of SQL export

--- Speech/schema.sql
+++ Speech/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Speech of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Speech_type AS ENUM ('Speech');
-CREATE TABLE Speech (if JSON, n TEXT, rt JSON, supportedLanguages JSON, supportedVoices TEXT, type Speech_type, utterance TEXT);
+CREATE TABLE Speech (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, supportedLanguages JSON, supportedVoices TEXT, type Speech_type, utterance TEXT);
