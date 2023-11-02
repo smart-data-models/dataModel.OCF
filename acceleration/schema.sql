c114d39c92da302c7f9be085ca755690220b6fb1
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:06:12 +0100

    test of SQL export

--- acceleration/schema.sql
+++ acceleration/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model acceleration of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE acceleration_type AS ENUM ('acceleration');
-CREATE TABLE acceleration (acceleration NUMERIC, if JSON, n TEXT, precision NUMERIC, range JSON, rt JSON, step NUMERIC, type acceleration_type);
+CREATE TABLE acceleration (acceleration NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, owner JSON, precision NUMERIC, range JSON, rt JSON, seeAlso JSON, source TEXT, step NUMERIC, type acceleration_type);
