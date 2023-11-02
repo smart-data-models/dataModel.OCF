2a863f9a0f2440e16cc119a7e8a95b9575e9fca2
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:06:28 +0100

    test of SQL export

--- Button/schema.sql
+++ Button/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Button of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Button_type AS ENUM ('Button');
-CREATE TABLE Button (if JSON, n TEXT, rt JSON, type Button_type, value BOOLEAN);
+CREATE TABLE Button (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, type Button_type, value BOOLEAN);
