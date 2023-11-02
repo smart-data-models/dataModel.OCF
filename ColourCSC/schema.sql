5373d9d055a2392d002e122b01c4e0bfa61924a0
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:06:34 +0100

    test of SQL export

--- ColourCSC/schema.sql
+++ ColourCSC/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ColourCSC of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ColourCSC_type AS ENUM ('ColourCSC');
-CREATE TABLE ColourCSC (csc JSON, if JSON, n TEXT, precision NUMERIC, rt JSON, type ColourCSC_type);
+CREATE TABLE ColourCSC (address JSON, alternateName TEXT, areaServed TEXT, csc JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, owner JSON, precision NUMERIC, rt JSON, seeAlso JSON, source TEXT, type ColourCSC_type);
