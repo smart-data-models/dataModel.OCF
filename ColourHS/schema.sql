2929620677eb9b18166dd77790875a2a7f354dba
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:06:36 +0100

    test of SQL export

--- ColourHS/schema.sql
+++ ColourHS/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ColourHS of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ColourHS_type AS ENUM ('ColourHS');
-CREATE TABLE ColourHS (hue NUMERIC, if JSON, maximumsaturation INTEGER, n TEXT, precision NUMERIC, rt JSON, saturation INTEGER, type ColourHS_type);
+CREATE TABLE ColourHS (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hue NUMERIC, id TEXT PRIMARY KEY, if JSON, location JSON, maximumsaturation NUMERIC, n TEXT, name TEXT, owner JSON, precision NUMERIC, rt JSON, saturation NUMERIC, seeAlso JSON, source TEXT, type ColourHS_type);
