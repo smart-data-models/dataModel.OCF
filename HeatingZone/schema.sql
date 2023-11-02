f568604758a33924c861464b32e8dae177e96da9
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:07:02 +0100

    test of SQL export

--- HeatingZone/schema.sql
+++ HeatingZone/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model HeatingZone of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE HeatingZone_type AS ENUM ('HeatingZone');
-CREATE TABLE HeatingZone (heatinglevel INTEGER, if JSON, maxheatinglevel INTEGER, n TEXT, rt JSON, type HeatingZone_type);
+CREATE TABLE HeatingZone (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, heatinglevel NUMERIC, id TEXT PRIMARY KEY, if JSON, location JSON, maxheatinglevel NUMERIC, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, type HeatingZone_type);
