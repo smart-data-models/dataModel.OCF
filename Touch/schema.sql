f1dbc7c29d18297a2ff61b67b5d69f1f95978f19
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:08:00 +0100

    test of SQL export

--- Touch/schema.sql
+++ Touch/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Touch of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Touch_type AS ENUM ('Touch');
-CREATE TABLE Touch (if JSON, n TEXT, rt JSON, type Touch_type, value BOOLEAN);
+CREATE TABLE Touch (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, type Touch_type, value BOOLEAN);
