016b1e1417d5582734290a6997a97766df0689c9
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:07:53 +0100

    test of SQL export

--- SoundPressure/schema.sql
+++ SoundPressure/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model SoundPressure of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SoundPressure_type AS ENUM ('SoundPressure');
-CREATE TABLE SoundPressure (if JSON, n TEXT, rt JSON, soundpa NUMERIC, type SoundPressure_type);
+CREATE TABLE SoundPressure (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, soundpa NUMERIC, source TEXT, type SoundPressure_type);
