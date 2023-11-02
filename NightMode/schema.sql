5071ace2413ed5576fca0b20065a8f59777ab3f0
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:07:26 +0100

    test of SQL export

--- NightMode/schema.sql
+++ NightMode/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model NightMode of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE NightMode_type AS ENUM ('NightMode');
-CREATE TABLE NightMode (if JSON, n TEXT, nightMode BOOLEAN, rt JSON, type NightMode_type);
+CREATE TABLE NightMode (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, nightMode BOOLEAN, owner JSON, rt JSON, seeAlso JSON, source TEXT, type NightMode_type);
