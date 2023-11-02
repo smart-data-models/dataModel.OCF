3a67e3d8f5ed7160c700cae4abc055dcdc25398d
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:06:38 +0100

    test of SQL export

--- consumable/schema.sql
+++ consumable/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model consumable of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE consumable_type AS ENUM ('consumable');
-CREATE TABLE consumable (if JSON, n TEXT, orderpercentage INTEGER, remaining INTEGER, rt JSON, type consumable_type, typeofconsumable TEXT, url TEXT);
+CREATE TABLE consumable (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, n TEXT, name TEXT, orderpercentage NUMERIC, owner JSON, remaining NUMERIC, rt JSON, seeAlso JSON, source TEXT, type consumable_type, typeofconsumable TEXT, url TEXT);
