7b3483778fca8125dbcff0f5af62490d2aaa1789
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:07:09 +0100

    test of SQL export

--- Lock/schema.sql
+++ Lock/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Lock of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE lockState_type AS ENUM ('Locked','Unlocked');CREATE TYPE Lock_type AS ENUM ('Lock');
-CREATE TABLE Lock (if JSON, lockState lockState_type, n TEXT, rt JSON, type Lock_type);
+CREATE TABLE Lock (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, lockState lockState_type, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, type Lock_type);
