386e9132d9512ba832eec038dc71d33030752dfc
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:07:19 +0100

    test of SQL export

--- Mode/schema.sql
+++ Mode/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Mode of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Mode_type AS ENUM ('Mode');
-CREATE TABLE Mode (if JSON, modes JSON, n TEXT, rt JSON, supportedModes JSON, type Mode_type);
+CREATE TABLE Mode (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, modes JSON, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, supportedModes JSON, type Mode_type);
