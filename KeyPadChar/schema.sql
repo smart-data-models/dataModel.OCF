8f5c9a6cc939bdcc31544900778a76eff88fb14f
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:07:08 +0100

    test of SQL export

--- KeyPadChar/schema.sql
+++ KeyPadChar/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model KeyPadChar of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE keyvalue_type AS ENUM ('0','1','2','3','4','5','6','7','8','9','*','#');CREATE TYPE KeyPadChar_type AS ENUM ('KeyPadChar');
-CREATE TABLE KeyPadChar (if JSON, keyvalue keyvalue_type, n TEXT, rt JSON, type KeyPadChar_type);
+CREATE TABLE KeyPadChar (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, keyvalue keyvalue_type, location JSON, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, type KeyPadChar_type);
