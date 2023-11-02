116a70168b801ae60c408ed45d1736cb3731d92d
Alberto Abella <alberto.abella@fiware.org>
2023-11-02 23:07:22 +0100

    test of SQL export

--- movement/schema.sql
+++ movement/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model movement of the subject dataModel.OCF for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE movement_type AS ENUM ('movement');
-CREATE TABLE movement (if JSON, movement TEXT, movementModifier TEXT, movementSettings JSON, n TEXT, rt JSON, type movement_type);
+CREATE TABLE movement (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, if JSON, location JSON, movement TEXT, movementModifier TEXT, movementSettings JSON, n TEXT, name TEXT, owner JSON, rt JSON, seeAlso JSON, source TEXT, type movement_type);
