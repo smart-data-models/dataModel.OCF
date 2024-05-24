25c9283c1e098d7635b24c9b16d1bd41d30b2e3e
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:32:55 +0200

    created example of code

--- 3DPrinter/code/code_for_using_dataModel.OCF_3DPrinter.py
+++ 3DPrinter/code/code_for_using_dataModel.OCF_3DPrinter.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "3DPrinter"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.printer.3d']}
+rt = ['oic.r.printer.3d']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-printsizey = {'type': 'Property', 'value': 528.5}
+printsizey = 528.5
 attribute = "printsizey"
 value = printsizey
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-memorysize = {'type': 'Property', 'value': 561.5}
+memorysize = 561.5
 attribute = "memorysize"
 value = memorysize
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-3dprinttype = "{'type': 'Property', 'value': 'Photopolymer Jetting Technology'}"
+3dprinttype = "Photopolymer Jetting Technology"
 attribute = "3dprinttype"
 value = 3dprinttype
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
