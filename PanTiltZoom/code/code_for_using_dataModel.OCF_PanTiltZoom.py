cf0fc8970453bc9b15ae6e271f7f8b250763f132
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:39:39 +0200

    created example of code

--- PanTiltZoom/code/code_for_using_dataModel.OCF_PanTiltZoom.py
+++ PanTiltZoom/code/code_for_using_dataModel.OCF_PanTiltZoom.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "PanTiltZoom"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.ptz']}
+rt = ['oic.r.ptz']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-tilt_range = {'type': 'Property', 'value': [972.1, 351.2]}
+tilt_range = [972.1, 351.2]
 attribute = "tilt_range"
 value = tilt_range
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-zoomFactor = "{'type': 'Property', 'value': 'Best onto college type pick.'}"
+zoomFactor = "Best onto college type pick."
 attribute = "zoomFactor"
 value = zoomFactor
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-tilt = {'type': 'Property', 'value': 122.5}
+tilt = 122.5
 attribute = "tilt"
 value = tilt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
