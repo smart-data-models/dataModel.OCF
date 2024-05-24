385349df2c7c54238cb4752213e1153f14a6afaa
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:37:41 +0200

    created example of code

--- ColourHS/code/code_for_using_dataModel.OCF_ColourHS.py
+++ ColourHS/code/code_for_using_dataModel.OCF_ColourHS.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "ColourHS"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.colour.hs']}
+rt = ['oic.r.colour.hs']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hue = {'type': 'Property', 'value': 311.1}
+hue = 311.1
 attribute = "hue"
 value = hue
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-saturation = {'type': 'Property', 'value': 25247}
+saturation = 25247
 attribute = "saturation"
 value = saturation
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-maximumsaturation = {'type': 'Property', 'value': 25247}
+maximumsaturation = 25247
 attribute = "maximumsaturation"
 value = maximumsaturation
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
