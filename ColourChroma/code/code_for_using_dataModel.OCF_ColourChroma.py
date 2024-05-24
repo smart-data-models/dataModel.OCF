d2d9fbf114b591468c6c1688fbcd3fe3c437a39d
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:37:34 +0200

    created example of code

--- ColourChroma/code/code_for_using_dataModel.OCF_ColourChroma.py
+++ ColourChroma/code/code_for_using_dataModel.OCF_ColourChroma.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "ColourChroma"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.colour.chroma']}
+rt = ['oic.r.colour.chroma']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-ct = {'type': 'Property', 'value': 864}
+ct = 864
 attribute = "ct"
 value = ct
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hue = {'type': 'Property', 'value': 80.9}
+hue = 80.9
 attribute = "hue"
 value = hue
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-saturation = {'type': 'Property', 'value': 25247}
+saturation = 25247
 attribute = "saturation"
 value = saturation
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
