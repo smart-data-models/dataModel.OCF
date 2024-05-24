993cbda8f833740b87bfd067d3f672c18994bfff
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:41:04 +0200

    created example of code

--- WaterInfo/code/code_for_using_dataModel.OCF_WaterInfo.py
+++ WaterInfo/code/code_for_using_dataModel.OCF_WaterInfo.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "WaterInfo"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.waterinfo']}
+rt = ['oic.r.waterinfo']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-supportedwatertypes = {'type': 'Property', 'value': ['Less dream off. Spring something western social.', 'Scene like speech surface huge land hand.']}
+supportedwatertypes = ['Less dream off. Spring something western social.', 'Scene like speech surface huge land hand.']
 attribute = "supportedwatertypes"
 value = supportedwatertypes
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-supportedadditivetypes = {'type': 'Property', 'value': ['Produce whatever team really. Support later artist seven wait ask.', 'Author start four common impact. Door form blue three. Art mean size.']}
+supportedadditivetypes = ['Produce whatever team really. Support later artist seven wait ask.', 'Author start four common impact. Door form blue three. Art mean size.']
 attribute = "supportedadditivetypes"
 value = supportedadditivetypes
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-currentwatertype = "{'type': 'Property', 'value': 'Chair wear production fill. Mention there box wide.'}"
+currentwatertype = "Chair wear production fill. Mention there box wide."
 attribute = "currentwatertype"
 value = currentwatertype
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
