0d7e0a9110961442a5c7451e0e3f87d04ced1d4b
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:35:09 +0200

    created example of code

--- HbA1c/code/code_for_using_dataModel.OCF_HbA1c.py
+++ HbA1c/code/code_for_using_dataModel.OCF_HbA1c.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "HbA1c"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.glucose.hba1c']}
+rt = ['oic.r.glucose.hba1c']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hba1c = {'type': 'Property', 'value': 8.0}
+hba1c = 8.0
 attribute = "hba1c"
 value = hba1c
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-range = {'type': 'Property', 'value': [765.7, 907.2]}
+range = [765.7, 907.2]
 attribute = "range"
 value = range
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-step = {'type': 'Property', 'value': 962.1}
+step = 962.1
 attribute = "step"
 value = step
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
