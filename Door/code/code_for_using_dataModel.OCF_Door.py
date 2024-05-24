69a3c7f241bc21ed82d248865c5d54ceb2544df9
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:38:06 +0200

    created example of code

--- Door/code/code_for_using_dataModel.OCF_Door.py
+++ Door/code/code_for_using_dataModel.OCF_Door.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Door"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.door']}
+rt = ['oic.r.door']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-openDuration = "{'type': 'Property', 'value': 'P1710168910W264733215084615551330344341353172274567622193828821450614981840335945689055955212929777261D'}"
+openDuration = "P1710168910W264733215084615551330344341353172274567622193828821450614981840335945689055955212929777261D"
 attribute = "openDuration"
 value = openDuration
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-openState = "{'type': 'Property', 'value': 'Open'}"
+openState = "Open"
 attribute = "openState"
 value = openState
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-openAlarm = {'type': 'Property', 'value': False}
+openAlarm = False
 attribute = "openAlarm"
 value = openAlarm
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
