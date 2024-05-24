85999b7ed7d77486dff178e629cf59bb4a10adf3
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:39:19 +0200

    created example of code

--- Mode/code/code_for_using_dataModel.OCF_Mode.py
+++ Mode/code/code_for_using_dataModel.OCF_Mode.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Mode"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.mode']}
+rt = ['oic.r.mode']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-modes = {'type': 'Property', 'value': ['Mouth personal notice. Do lette', 'Lawyer situation single huge. Up simply fine itself themsel']}
+modes = ['Mouth personal notice. Do lette', 'Lawyer situation single huge. Up simply fine itself themsel']
 attribute = "modes"
 value = modes
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-supportedModes = {'type': 'Property', 'value': ['Decision already serious nature anyone peace ago. Baby but onto region.', 'Focus hotel smile record. Risk around dream second dream quality onto.']}
+supportedModes = ['Decision already serious nature anyone peace ago. Baby but onto region.', 'Focus hotel smile record. Risk around dream second dream quality onto.']
 attribute = "supportedModes"
 value = supportedModes
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-n = "{'type': 'Property', 'value': 'Fire customer surface. Like'}"
+n = "Fire customer surface. Like"
 attribute = "n"
 value = n
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
