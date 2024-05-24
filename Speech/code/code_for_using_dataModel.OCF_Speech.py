06f552918e0af72d85dbca9171eb72d40c57667e
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:40:37 +0200

    created example of code

--- Speech/code/code_for_using_dataModel.OCF_Speech.py
+++ Speech/code/code_for_using_dataModel.OCF_Speech.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Speech"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.speech.tts']}
+rt = ['oic.r.speech.tts']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-supportedLanguages = {'type': 'Property', 'value': ['Employee blood hospital my impact. Small suggest now lawyer.', 'Side teach quit']}
+supportedLanguages = ['Employee blood hospital my impact. Small suggest now lawyer.', 'Side teach quit']
 attribute = "supportedLanguages"
 value = supportedLanguages
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-supportedVoices = "{'type': 'Property', 'value': 'Development less court else dark know. Couple less none ago order certainly film. House help hospital east.'}"
+supportedVoices = "Development less court else dark know. Couple less none ago order certainly film. House help hospital east."
 attribute = "supportedVoices"
 value = supportedVoices
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-utterance = "{'type': 'Property', 'value': 'Style themselves keep follow exist. Voice produce candidate thought total.'}"
+utterance = "Style themselves keep follow exist. Voice produce candidate thought total."
 attribute = "utterance"
 value = utterance
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
