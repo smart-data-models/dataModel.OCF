1aa38628b2d0977adb02ec6c5aa056b89e5ca9cc
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:33:03 +0200

    created example of code

--- AirFlow/code/code_for_using_dataModel.OCF_AirFlow.py
+++ AirFlow/code/code_for_using_dataModel.OCF_AirFlow.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "AirFlow"
 subject = "dataModel.OCF"
-rt = {'type': 'Property', 'value': ['oic.r.airflow']}
+rt = ['oic.r.airflow']
 attribute = "rt"
 value = rt
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-speed = {'type': 'Property', 'value': 864}
+speed = 864
 attribute = "speed"
 value = speed
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-direction = "{'type': 'Property', 'value': 'American whole magazine truth stop whose. On traditional measure example sense peace. Would mouth relate own chair.'}"
+direction = "American whole magazine truth stop whose. On traditional measure example sense peace. Would mouth relate own chair."
 attribute = "direction"
 value = direction
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-automode = "{'type': 'Property', 'value': 'On'}"
+automode = "On"
 attribute = "automode"
 value = automode
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
