8af622fe027b88da1927eb5d45c798b2440ac962
Alberto Abella <alberto.abella@fiware.org>
2024-08-13 18:28:45 +0200

    Draft version of pydantic file v2

--- Activity/code/code_for_using_pydantic.py
+++ Activity/code/code_for_using_pydantic.py
@@ -1,1 +1,275 @@
-None
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import (
+    AnyUrl,
+    AwareDatetime,
+    BaseModel,
+    Field,
+    RootModel,
+    confloat,
+    constr,
+)
+
+
+class Activity1(Enum):
+    sleep = 'sleep'
+    sit = 'sit'
+    stand = 'stand'
+    walk = 'walk'
+    run = 'run'
+    unknown = 'unknown'
+
+
+class Address(BaseModel):
+    addressCountry: Optional[str] = Field(
+        None, description='The country. For example, Spain'
+    )
+    addressLocality: Optional[str] = Field(
+        None,
+        description='The locality in which the street address is, and which is in the region',
+    )
+    addressRegion: Optional[str] = Field(
+        None,
+        description='The region in which the locality is, and which is in the country',
+    )
+    district: Optional[str] = Field(
+        None,
+        description='A district is a type of administrative division that, in some countries, is managed by the local government',
+    )
+    postOfficeBoxNumber: Optional[str] = Field(
+        None,
+        description='The post office box number for PO box addresses. For example, 03578',
+    )
+    postalCode: Optional[str] = Field(
+        None, description='The postal code. For example, 24004'
+    )
+    streetAddress: Optional[str] = Field(None, description='The street address')
+    streetNr: Optional[str] = Field(
+        None, description='Number identifying a specific property on a public street'
+    )
+
+
+class IfEnum(Enum):
+    oic_if_s = 'oic.if.s'
+    oic_if_baseline = 'oic.if.baseline'
+
+
+class Type(Enum):
+    Point = 'Point'
+
+
+class Location(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[float] = Field(..., min_length=2)
+    type: Type
+
+
+class Coordinate(RootModel[List[float]]):
+    root: List[float]
+
+
+class Type1(Enum):
+    LineString = 'LineString'
+
+
+class Location1(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[Coordinate] = Field(..., min_length=2)
+    type: Type1
+
+
+class Type2(Enum):
+    Polygon = 'Polygon'
+
+
+class Location2(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type2
+
+
+class Type3(Enum):
+    MultiPoint = 'MultiPoint'
+
+
+class Location3(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[float]]
+    type: Type3
+
+
+class Type4(Enum):
+    MultiLineString = 'MultiLineString'
+
+
+class Location4(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type4
+
+
+class Type5(Enum):
+    MultiPolygon = 'MultiPolygon'
+
+
+class Location5(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[List[Coordinate]]]
+    type: Type5
+
+
+class RtEnum(Enum):
+    oic_r_activity = 'oic.r.activity'
+
+
+class Type6(Enum):
+    Activity = 'Activity'
+
+
+class Activity(BaseModel):
+    activity: Optional[Activity1] = Field(
+        None,
+        description='This Property describes the recognized current activity type of user',
+    )
+    address: Optional[Address] = Field(None, description='The mailing address')
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    areaServed: Optional[str] = Field(
+        None,
+        description='The geographic area where a service or offered item is provided',
+    )
+    ccal_day: Optional[confloat(ge=0.0)] = Field(
+        None,
+        description='This Property describes the burned off calories of user since the beginning of the day',
+    )
+    ccal_day_precision: Optional[float] = Field(
+        None,
+        description="When exposed the value in 'precision' provides a +/- tolerance against the Properties in the Resource. Thus if a Property is UPDATED to a value and that Property then RETRIEVED, the RETRIEVED value is valid if in the range of the set value +/- precision",
+    )
+    ccal_day_range: Optional[List[float]] = Field(
+        None,
+        description='The valid range for the Property in the Resource as a number. The first value in the array is the minimum value, the second value in the array is the maximum value',
+        max_length=2,
+        min_length=2,
+    )
+    ccal_day_step: Optional[float] = Field(
+        None,
+        description='Step value across the defined range an integer when the range is a number.  This is the increment for valid values across the range; so if range is 0.0..10.0 and step is 2.5 then valid values are 0.0,2.5,5.0,7.5,10.0',
+    )
+    ccal_reset: Optional[confloat(ge=0.0)] = Field(
+        None,
+        description='This Property describes the burned off calories of user since the last reset',
+    )
+    ccal_reset_precision: Optional[float] = Field(
+        None,
+        description="When exposed the value in 'precision' provides a +/- tolerance against the Properties in the Resource. Thus if a Property is UPDATED to a value and that Property then RETRIEVED, the RETRIEVED value is valid if in the range of the set value +/- precision",
+    )
+    ccal_reset_range: Optional[List[float]] = Field(
+        None,
+        description='The valid range for the Property in the Resource as a number. The first value in the array is the minimum value, the second value in the array is the maximum value',
+        max_length=2,
+        min_length=2,
+    )
+    ccal_reset_step: Optional[float] = Field(
+        None,
+        description='Step value across the defined range an integer when the range is a number.  This is the increment for valid values across the range; so if range is 0.0..10.0 and step is 2.5 then valid values are 0.0,2.5,5.0,7.5,10.0',
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    if_: Optional[List[IfEnum]] = Field(
+        None,
+        alias='if',
+        description='The OCF Interface set supported by this Resource',
+        min_length=1,
+    )
+    location: Optional[
+        Union[Location, Location1, Location2, Location3, Location4, Location5]
+    ] = Field(
+        None,
+        description='Geojson reference to the item. It can be Point, LineString, Polygon, MultiPoint, MultiLineString or MultiPolygon',
+    )
+    n: Optional[constr(max_length=64)] = Field(
+        None, description='Friendly name of the Resource'
+    )
+    name: Optional[str] = Field(None, description='The name of this item')
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    rt: Optional[List[RtEnum]] = Field(
+        None, description='The Resource Type', min_length=1
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='A sequence of characters giving the original source of the entity data as a URL. Recommended to be the fully qualified domain name of the source provider, or the URL to the source object',
+    )
+    steps_day: Optional[confloat(ge=0.0)] = Field(
+        None,
+        description="This Property describes the user's step count that measures the number of steps the user has taken since the beginning of the day",
+    )
+    steps_day_range: Optional[List[int]] = Field(
+        None,
+        description='The valid range for the Property in the Resource as an integer. The first value in the array is the minimum value, the second value in the array is the maximum value',
+        max_length=2,
+        min_length=2,
+    )
+    steps_day_step: Optional[float] = Field(
+        None,
+        description='Step value across the defined range when the range is an integer.  This is the increment for valid values across the range; so if range is 0..10 and step is 2 then valid values are 0,2,4,6,8,10',
+    )
+    steps_reset: Optional[confloat(ge=0.0)] = Field(
+        None,
+        description="This Property describes the user's step count that measures the number of steps the user has taken since the last reset",
+    )
+    steps_reset_range: Optional[List[int]] = Field(
+        None,
+        description='The valid range for the Property in the Resource as an integer. The first value in the array is the minimum value, the second value in the array is the maximum value',
+        max_length=2,
+        min_length=2,
+    )
+    steps_reset_step: Optional[float] = Field(
+        None,
+        description='Step value across the defined range when the range is an integer.  This is the increment for valid values across the range; so if range is 0..10 and step is 2 then valid values are 0,2,4,6,8,10',
+    )
+    type: Optional[Type6] = Field(
+        None, description='NGSI entity type. It has to be Activity'
+    )
