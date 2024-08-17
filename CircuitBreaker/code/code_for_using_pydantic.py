f0097d003aafb4699b9017fbd41d60c87cf8c79d
Alberto Abella <alberto.abella@fiware.org>
2024-08-18 00:55:32 +0200

    Draft version of pydantic file v2

---
+++ CircuitBreaker/code/code_for_using_pydantic.py
@@ -0,0 +1,220 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import AnyUrl, AwareDatetime, BaseModel, Field, RootModel, constr
+
+
+class Address(BaseModel):
+    addressCountry: Optional[str] = Field(
+        None, description='The country. For example, Spain'
+    )
+    addressLocality: Optional[str] = Field(
+        None,
+        description='The locality in which the street address is, and which is in the region',
+    )
+    addressRegion: Optional[str] = Field(
+        None,
+        description='The region in which the locality is, and which is in the country',
+    )
+    district: Optional[str] = Field(
+        None,
+        description='A district is a type of administrative division that, in some countries, is managed by the local government',
+    )
+    postOfficeBoxNumber: Optional[str] = Field(
+        None,
+        description='The post office box number for PO box addresses. For example, 03578',
+    )
+    postalCode: Optional[str] = Field(
+        None, description='The postal code. For example, 24004'
+    )
+    streetAddress: Optional[str] = Field(None, description='The street address')
+    streetNr: Optional[str] = Field(
+        None, description='Number identifying a specific property on a public street'
+    )
+
+
+class IfEnum(Enum):
+    oic_if_s = 'oic.if.s'
+    oic_if_baseline = 'oic.if.baseline'
+
+
+class Type(Enum):
+    Point = 'Point'
+
+
+class Location(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[float] = Field(..., min_length=2)
+    type: Type
+
+
+class Coordinate(RootModel[List[float]]):
+    root: List[float]
+
+
+class Type1(Enum):
+    LineString = 'LineString'
+
+
+class Location1(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[Coordinate] = Field(..., min_length=2)
+    type: Type1
+
+
+class Type2(Enum):
+    Polygon = 'Polygon'
+
+
+class Location2(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type2
+
+
+class Type3(Enum):
+    MultiPoint = 'MultiPoint'
+
+
+class Location3(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[float]]
+    type: Type3
+
+
+class Type4(Enum):
+    MultiLineString = 'MultiLineString'
+
+
+class Location4(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type4
+
+
+class Type5(Enum):
+    MultiPolygon = 'MultiPolygon'
+
+
+class Location5(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[List[Coordinate]]]
+    type: Type5
+
+
+class RtEnum(Enum):
+    oic_r_circuitbreaker = 'oic.r.circuitbreaker'
+
+
+class Status(Enum):
+    on = 'on'
+    off = 'off'
+    trip = 'trip'
+
+
+class Type6(Enum):
+    CircuitBreaker = 'CircuitBreaker'
+
+
+class CircuitBreaker(BaseModel):
+    address: Optional[Address] = Field(None, description='The mailing address')
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    areaServed: Optional[str] = Field(
+        None,
+        description='The geographic area where a service or offered item is provided',
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    if_: Optional[List[IfEnum]] = Field(
+        None,
+        alias='if',
+        description='The OCF Interface set supported by this Resource',
+        min_length=2,
+    )
+    insulationresistance: Optional[float] = Field(
+        None, description='Insulation resistance of circuit breaker (M Ohm)'
+    )
+    leakagecurrent: Optional[float] = Field(
+        None, description='The leakage current in mA'
+    )
+    location: Optional[
+        Union[Location, Location1, Location2, Location3, Location4, Location5]
+    ] = Field(
+        None,
+        description='Geojson reference to the item. It can be Point, LineString, Polygon, MultiPoint, MultiLineString or MultiPolygon',
+    )
+    n: Optional[constr(max_length=64)] = Field(
+        None, description='Friendly name of the Resource'
+    )
+    name: Optional[str] = Field(None, description='The name of this item')
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    ratedbreakingcurrent: Optional[float] = Field(
+        None,
+        description='The rated breaking current in Ampere, defined at manufacturing time',
+    )
+    ratedcurrent: Optional[float] = Field(
+        None, description='The rated current in Ampere, defined at manufacturing time'
+    )
+    ratedvoltage: Optional[float] = Field(
+        None, description='The rated voltage in Volts, defined at manufacturing time'
+    )
+    rt: Optional[List[RtEnum]] = Field(
+        None, description='The Resource Type', min_length=1
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='A sequence of characters giving the original source of the entity data as a URL. Recommended to be the fully qualified domain name of the source provider, or the URL to the source object',
+    )
+    status: Optional[Status] = Field(
+        None,
+        description='The circuit breaker status. The status can only be reset out of bounds',
+    )
+    timestamp: Optional[AwareDatetime] = Field(
+        None,
+        description='An RFC3339 formatted time indicating when the data was observed (e.g.: 2016-02-15T09:19Z, 1996-12-19T16:39:57-08:00). Note that 1/100 time resolution should be used',
+    )
+    type: Optional[Type6] = Field(
+        None, description='NGSI entity type. It has to be CircuitBreaker'
+    )
